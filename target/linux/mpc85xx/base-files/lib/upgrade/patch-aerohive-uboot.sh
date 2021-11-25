880c1700adf1e90788c4dc98d4c38c9cc5e57eb4
Martin Kennedy <hurricos@gmail.com>
2021-11-24 23:35:52 -0500

    mpc85xx: Add Aerohive HiveAP-370 Access Point
    This patch adds support for the Aerohive HiveAP 370. It is very similar to
    the HiveAP-330.
    
    Specifications:
     * SoC: QorIQ P1020 e500v2 @ 800MHz
     * RAM: 512 MB
     * Flash: 1024 MB NAND flash
     * WiFi: Atheros QCA9890 (5 GHz) and AR9590 (2.4 GHz)
     * Ethernet: 2 x 1000Base-T (PoE)
     * LED: One combined activity LED (orange/white/off)
     * UART: 9600,8,N,1 RJ45 console
    
    Installation:
     * Installation via console serial tftp over ethernet
       + Connect to the RJ45-based serial console port (see https://openwrt.org/docs/techref/hardware/port.serial)
       + Use miniterm/putty etc to connect 9600,8,N,1 (miniterm.py /dev/ttyS0 --raw --eol CR)
       + Power AP370 with PoE or 12V DC barrel jack
       + Hit space to interupt the boot process
       + Enter uboot password AhNf?d@ta06 or administrator
       + Enter the following commands to boot from tftp image:
    
         dhcp;
         setenv serverip <TFTP server IPv4 address>;
         tftpboot 0x1000000 openwrt-mpc85xx-p1020-aerohive_hiveap-370-initramfs-kernel.bin;
         tftpboot 0x6000000 openwrt-mpc85xx-p1020-aerohive_hiveap-370-squashfs-fdt.bin;
    
         setenv owrt_boot_a 'nand read 0x6000000 0x00520000 0x40000\; nand read 0x1000000 0x560000 0x02000000\; bootm 0x1000000 - 0x6000000\;'
         saveenv;
    
         bootm 0x1000000 - 0x6000000;
    
       + Once booted, patch your u-boot installation so that it runs
         `owrt_boot_a` on boot by doing:
    
         . /lib/upgrade/patch-aerohive-uboot.sh;
    	 patch_aerohive_hiveap_370_uboot;
    
       + Finally, wget or scp over the sysupgrade file under /tmp, and
         sysupgrade with:
    
         sysupgrade -v /tmp/openwrt-mpc85xx-p1020-aerohive_hiveap-370-squashfs-sysupgrade.bin
    
    Notes:
    
    * The initial format of the ubi filesystem will take a long time, so
      please be patient.
    
    * A/B partitioning functionality has been dropped: both copies of
      U-boot run `owrt_boot_a` to boot.
    
    * There is no 'official' way to possible to revert to the original
      firmware, but this should be of no concern, as the device is EoL on
      the original FW:
      https://aerohive-www-cdn.aerohive.com/wp-content/uploads/Aerohive_EOS-EOL_AP370-AP390.pdf
    
    * The kernel partition contains a shimmed dtb to allow for the use of
      sysupgrade-tar.
    
    * The MAC address is pulled from NAND flash using nvmem
    
      We actually need to enclose the whole section of partitions in a
      `partitions { ... }` because we need to assign at least the hwinfo
      partition a `compatible = "fixed-partitions";`; otherwise it won't
      be registered when bringing up MTD, as per these links:
    
      + https://forum.openwrt.org/t/tp-link-c2600-missing-default-mac-mtd-partition-in-snapshot/103945/6
    
      + Commit 67efc978cce2
    
      + Commit 11d425f1863b
    
    Signed-off-by: Martin Kennedy <hurricos@gmail.com>

---
+++ target/linux/mpc85xx/base-files/lib/upgrade/patch-aerohive-uboot.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Copyright (C) 2021 OpenWrt.org
+#
+
+. /lib/functions.sh
+. /lib/functions/system.sh
+
+patch_aerohive_hiveap_370_uboot() {
+	modinfo mtd-rw || {
+		echo "You must have kmod-mtd-rw installed to do this." >&2;
+		return 1;
+	}
+
+	tmpd=/tmp/uboot_patch;
+	mkdir -p ${tmpd}
+
+	local uboot1=$(find_mtd_index Uboot);
+	local uboot2=$(find_mtd_index Uboot-backup);
+
+	for part in Uboot Uboot-backup; do
+		local index=$(find_mtd_index ${part});
+		[ ! "$index" ] && { echo "No such index for part ${part}; panicking!" >&2 ; return 1; };
+
+		# Back up
+		dd if=/dev/mtd${index} of=${tmpd}/mtd${index};
+
+		# Patch
+		cp ${tmpd}/mtd${index} ${tmpd}/mtd${index}_patched;
+		strings -td < ${tmpd}/mtd${index} | grep '^ *[0-9]* *\(run owrt_boot\|setenv bootargs\).*nand read' |
+			awk '{print $1}' |
+			while read offset; do
+				echo -n "run owrt_boot_a;          " | dd of=${tmpd}/mtd${index}_patched bs=1 seek=${offset} conv=notrunc
+			done;
+
+		# Write
+		insmod mtd-rw i_want_a_brick=y
+		mtd erase /dev/mtd${index}
+		mtd write ${tmpd}/mtd${index}_patched /dev/mtd${index}
+
+		# Verify
+		cmp /dev/mtd${index} ${tmpd}/mtd${index}_patched || {
+			echo "Warning: written media differs from write source!" >&2;
+			return 2;
+		}
+	done
+}
